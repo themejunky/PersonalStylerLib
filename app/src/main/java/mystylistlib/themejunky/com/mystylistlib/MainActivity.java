4e300aea7c78448464d45e84590f9bff1dcafbb5
Alin Lastun <alinlastun@gmail.com>
2018-03-21 15:21:32 +0200

    added pressed state on borderImage

--- app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
+++ app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
@@ -7,14 +7,21 @@ import android.view.View;
 import com.themejunky.personalstylerlib.InitCaligraphy;
 import com.themejunky.personalstylerlib.customviews.CustomButton;
 import com.themejunky.personalstylerlib.customviews.CustomButtonBorder;
+import com.themejunky.personalstylerlib.customviews.CustomButtonBorderImage;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private CustomButtonBorderImage button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new InitCaligraphy("Catamaran-Regular.ttf");
+        /*
+        button = findViewById(R.id.test);
+        button.setTooltipText();*/
     }
+
 }
 
--- personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomButtonBorderImage.java
+++ personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomButtonBorderImage.java
@@ -3,6 +3,7 @@ package com.themejunky.personalstylerlib.customviews;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -14,15 +15,47 @@ import com.themejunky.personalstylerlib.R;
  * Created by Junky2 on 3/21/2018.
  */
 
-public class CustomButtonBorderImage extends BaseCustom_LinearLayout {
+public class CustomButtonBorderImage extends BaseCustom_LinearLayout implements View.OnTouchListener {
 
     protected View mDevider;
     protected TextView mText;
     protected ImageView mImageView;
     protected LinearLayout mContainer;
+    private Custom_BorderImage_Interface mListener;
+    private int mDefaultBackgroundColor,mDefaultBorderRadius, mDefaultBorderStroke,mDefaultBorderColorPressed,mDefaultBorderColorUnPressed;
+
+
+
+
+    /* click listener interface */
+    public interface Custom_BorderImage_Interface {
+        void onCustomBorderImageClick(View view);
+    }
+    /* setter for listener */
+    public void setListener(CustomButtonBorderImage.Custom_BorderImage_Interface nListener) {
+        mListener = nListener;
+    }
 
     public CustomButtonBorderImage(Context context) {
         super(context);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            setPressedState();
+            return true;
+        }else if (event.getAction() == MotionEvent.ACTION_UP){
+            setUnPressedState();
+            try {
+                mListener.onCustomBorderImageClick(v);
+            } catch (Exception e) {
+                Log.w(TAG, e.getMessage());
+            }
+        }
+        return false;
+
     }
 
     public CustomButtonBorderImage(Context nContext, AttributeSet nAttrs) {
@@ -35,6 +68,15 @@ public class CustomButtonBorderImage extends BaseCustom_LinearLayout {
         mText = findViewById(R.id.nText);
         mImageView = findViewById(R.id.nImageView);
         mContainer = findViewById(R.id.nContainer);
+        mContainer.setOnTouchListener(this);
+
+        mDefaultBackgroundColor = getResources().getColor(R.color.CustomButtonBorderImage_default_background_color);
+        mDefaultBorderColorUnPressed = getResources().getColor(R.color.CustomButtonBorderImage_default_border_color_unpresssed);
+        mDefaultBorderColorPressed = getResources().getColor(R.color.CustomButtonBorderImage_default_border_color_pressed);
+        mDefaultBorderRadius = getResources().getInteger(R.integer.cbbi_radius);
+        mDefaultBorderStroke = getResources().getInteger(R.integer.cbbi_stroke);
+
+        setDefaultState();
 
 
 
@@ -45,7 +87,6 @@ public class CustomButtonBorderImage extends BaseCustom_LinearLayout {
         setTitle(mText,R.styleable.CustomButtonBorderImage_cbbi_text);
         setTextColor(mText,R.styleable.CustomButtonBorderImage_cbbi_text_color,R.color.Appoint_Trigger_Border);
         mImageView.setImageDrawable(mTypedarray.getDrawable(R.styleable.CustomButtonBorderImage_cbbi_image));
-        mContainer.setBackground(mTypedarray.getDrawable(R.styleable.CustomButtonBorderImage_cbbi_background));
         mContainer.setPadding(mTypedarray.getInt(R.styleable.CustomButtonBorderImage_cbbi_padding,0),mTypedarray.getInt(R.styleable.CustomButtonBorderImage_cbbi_padding,0),mTypedarray.getInt(R.styleable.CustomButtonBorderImage_cbbi_padding,0),mTypedarray.getInt(R.styleable.CustomButtonBorderImage_cbbi_padding,0));
         //setPadding(mContainer,mTypedarray.getInt(R.styleable.CustomButtonBorderImage_cbbi_padding,0),getResources().getInteger(R.integer.ci_padping));
 
@@ -53,5 +94,29 @@ public class CustomButtonBorderImage extends BaseCustom_LinearLayout {
 
 
 
+    }
+
+    public void setPressedState(){
+        setBorderColorAndRadius(mContainer,
+                R.styleable.CustomButtonBorderImage_cbbi_backgroundColor,mDefaultBackgroundColor,
+                R.styleable.CustomButtonBorderImage_cbbi_border_radius,mDefaultBorderRadius,
+                R.styleable.CustomButtonBorderImage_cbbi_border_stroke,mDefaultBorderStroke,
+                R.styleable.CustomButtonBorderImage_cbbi_border_pressed,mDefaultBorderColorPressed);
+    }
+
+    public void setUnPressedState(){
+        setBorderColorAndRadius(mContainer,
+                R.styleable.CustomButtonBorderImage_cbbi_backgroundColor,mDefaultBackgroundColor,
+                R.styleable.CustomButtonBorderImage_cbbi_border_radius,mDefaultBorderRadius,
+                R.styleable.CustomButtonBorderImage_cbbi_border_stroke,mDefaultBorderStroke,
+                R.styleable.CustomButtonBorderImage_cbbi_border_unpressed,mDefaultBorderColorUnPressed);
+    }
+
+    public void setDefaultState() {
+        setBorderColorAndRadius(mContainer,
+                R.styleable.CustomButtonBorderImage_cbbi_backgroundColor,mDefaultBackgroundColor,
+                R.styleable.CustomButtonBorderImage_cbbi_border_radius,mDefaultBorderRadius,
+                R.styleable.CustomButtonBorderImage_cbbi_border_stroke,mDefaultBorderStroke,
+                R.styleable.CustomButtonBorderImage_cbbi_border_unpressed,mDefaultBorderColorUnPressed);
     }
 }
