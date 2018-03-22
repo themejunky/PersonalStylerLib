8435fc29f73c1cfbc1ba6b1299ac74916c2797ce
Alin Lastun <alinlastun@gmail.com>
2018-03-22 15:57:46 +0200

    changed border customInput

--- app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
+++ app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
@@ -1,26 +1,51 @@
 package mystylistlib.themejunky.com.mystylistlib;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.themejunky.personalstylerlib.InitCaligraphy;
 import com.themejunky.personalstylerlib.customviews.CustomButton;
 import com.themejunky.personalstylerlib.customviews.CustomButtonBorder;
 import com.themejunky.personalstylerlib.customviews.CustomButtonBorderImage;
+import com.themejunky.personalstylerlib.customviews.CustomInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity  {
 
-    private CustomButtonBorderImage button;
 
+    private CustomInput mSpinner;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new InitCaligraphy("Catamaran-Regular.ttf");
-        /*
-        button = findViewById(R.id.test);
-        button.setTooltipText();*/
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("Catamaran-Regular.ttf").setFontAttrId(R.attr.fontPath).build());
+        mSpinner=  findViewById(R.id.nSpinnerTest);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) mSpinner.findViewById(R.id.nSpinner)).setAdapter(dataAdapter);
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
 }
--- personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomInput.java
+++ personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomInput.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.Scroller;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.themejunky.personalstylerlib.R;
@@ -18,27 +20,28 @@ import java.lang.reflect.Type;
 
 /**
  * Default Custom Input used in all forms
- * @app:ci_type                 - set the type of the input (spinner or edit text)
- * @app:ci_keyboard             - set the input type of the keyboard text (text,min_text,email,phone,password,numbers)
- * @app:ci_title_style          - set text style class for title [R.style.ci_default_style]
- * @app:ci_style                - set text style class for edittext [R.style.ci_default_style]
- * @app:ci_font                 - set the font name of input (edittext)
- * @app:ci_title_font           - set the font name
- * @app:ci_title_text           - set the title text of the input
- * @app:ci_imei_option          - imei option ( "done" - close the keyboard ; "next" move cursor to the next input )
- * @app:ci_botton_line_default  - set bottom line color (default) [R.color.CustomInput_line_default]
- * @app:ci_botton_line_active   - set bottom line color (active) [R.color.CustomService_default_border_color_inactive]
- * @app:ci_botton_line_error    - set bottom line color (error) [R.color.ci_botton_line_error]
- * @app:ci_hint                 - set edittext hint
- * @app:ci_error_mandatory      - set the "*" to flag up the field is mandatory
- * @app:ci_error_message        - set the error message if validation not succeed
+ *
+ * @app:ci_type - set the type of the input (spinner or edit text)
+ * @app:ci_keyboard - set the input type of the keyboard text (text,min_text,email,phone,password,numbers)
+ * @app:ci_title_style - set text style class for title [R.style.ci_default_style]
+ * @app:ci_style - set text style class for edittext [R.style.ci_default_style]
+ * @app:ci_font - set the font name of input (edittext)
+ * @app:ci_title_font - set the font name
+ * @app:ci_title_text - set the title text of the input
+ * @app:ci_imei_option - imei option ( "done" - close the keyboard ; "next" move cursor to the next input )
+ * @app:ci_botton_line_default - set bottom line color (default) [R.color.CustomInput_line_default]
+ * @app:ci_botton_line_active - set bottom line color (active) [R.color.CustomService_default_border_color_inactive]
+ * @app:ci_botton_line_error - set bottom line color (error) [R.color.ci_botton_line_error]
+ * @app:ci_hint - set edittext hint
+ * @app:ci_error_mandatory - set the "*" to flag up the field is mandatory
+ * @app:ci_error_message - set the error message if validation not succeed
  */
 public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocusChangeListener {
 
-    private EditText mInput,mInputBorder,mInputMultiLine;
-    private String mInputType, mInputKeyboard,mGravity,mScrollbar;
+    private EditText mInput, mInputBorder;
+    private String mInputType, mInputKeyboard, mGravity, mScrollbar;
     private TextView mError;
-    private int mDefaultBackgroundColor,mDefaultBorderRadius, mDefaultBorderStroke, mDefaultBorderColorFocus, mDefaultBorderColorUnFocus;
+    private int mDefaultBackgroundColor, mDefaultBorderRadius, mDefaultBorderStroke, mDefaultBorderColorFocus, mDefaultBorderColorUnFocus;
 
     public CustomInput(Context nContext, AttributeSet nAttrs) {
         super(nContext, nAttrs);
@@ -46,11 +49,11 @@ public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocus
         mTypedarray = nContext.obtainStyledAttributes(nAttrs, R.styleable.CustomInput);
         inflate(nContext, R.layout.custom_input_edit_text, this);
         mInputType = mTypedarray.getString(R.styleable.CustomInput_ci_type);
-
+        mInput = findViewById(R.id.nInput);
         TextView mTitle = findViewById(R.id.nTitle);
-            setStyle(mTitle, R.styleable.CustomInput_ci_title_style, R.style.ci_default_style);
-            setFontFamily(mTitle, mTypedarray.getString(R.styleable.CustomInput_ci_title_font), true);
-            setTitle(mTitle, R.styleable.CustomInput_ci_title_text);
+        setStyle(mTitle, R.styleable.CustomInput_ci_title_style, R.style.ci_default_style);
+        setFontFamily(mTitle, mTypedarray.getString(R.styleable.CustomInput_ci_title_font), true);
+        setTitle(mTitle, R.styleable.CustomInput_ci_title_text);
 
         mDefaultBackgroundColor = getResources().getColor(R.color.CustomInput_default_background_color);
         mDefaultBorderColorUnFocus = getResources().getColor(R.color.CustomInput_default_border_color_unfocus);
@@ -59,40 +62,31 @@ public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocus
         mDefaultBorderStroke = getResources().getInteger(R.integer.ci_stroke);
 
 
-
-
         if (mInputType.equals("1")) {
-            mInput = findViewById(R.id.nInput);
-            mInput.setVisibility(View.VISIBLE);
-            setEditTextParamiters(mInput);
 
-        }else if (mInputType.equals("2")) {
+            mInput.setVisibility(View.VISIBLE);
+            setEditTextParameters(mInput);
+        } else if (mInputType.equals("2")) {
             CustomSpinner mSpinner = findViewById(R.id.nCustomSpinner);
             mSpinner.setVisibility(View.VISIBLE);
+            mSpinner.setOnFocusChangeListener(this);
+        } else if (mInputType.equals("3")) {
+            mInput.setVisibility(View.VISIBLE);
+            setEditTextParameters(mInput);
+
+            setMinLines(mInput, R.styleable.CustomInput_ci_min_lines, getResources().getInteger(R.integer.ci_min_lines));
+            setMaxLines(mInput, R.styleable.CustomInput_ci_max_lines, getResources().getInteger(R.integer.ci_max_lines));
+            setGravity(mInput, R.styleable.CustomInput_ci_gravity, Gravity.TOP | Gravity.LEFT);
+            setScrollBar(mInput, R.styleable.CustomInput_ci_scrollbars);
+            mInput.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+            mInput.setSingleLine(false);
         }
-        else if (mInputType.equals("3")) {
-            mInputMultiLine = findViewById(R.id.inputMultiLine);
-            setEditTextParamiters(mInputMultiLine);
 
-            mInputMultiLine.setVisibility(View.VISIBLE);
-
-            setMinLines(mInputMultiLine,R.styleable.CustomInput_ci_min_lines,getResources().getInteger(R.integer.ci_min_lines));
-            setMaxLines(mInputMultiLine,R.styleable.CustomInput_ci_max_lines,getResources().getInteger(R.integer.ci_max_lines));
-            setGravity(mInputMultiLine,R.styleable.CustomInput_ci_gravity,Gravity.TOP | Gravity.LEFT);
-            setScrollBar(mInputMultiLine,R.styleable.CustomInput_ci_scrollbars);
-
-            mInputMultiLine.setSingleLine(false);
-            mInputMultiLine.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
-
-        }
 
         mError = findViewById(R.id.nError);
         setTitle(mError, R.styleable.CustomInput_ci_error_message);
-
         TextView mMandatory = findViewById(R.id.nMandatory);
         setMandatory(mMandatory, R.styleable.CustomInput_ci_error_mandatory);
-
-
     }
 
     /**
@@ -100,13 +94,11 @@ public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocus
      * else default gray
      */
     @Override
-    public void onFocusChange(View view, boolean b) {
+    public void onFocusChange(View nView, boolean b) {
         if (b) {
-            setInputBottomLineColor((TextView) findViewById(R.id.nInput), R.styleable.CustomInput_ci_botton_line_active, R.color.CustomInput_line_active);
-            setFocusState(view);
+           setFocusState(nView);
         } else {
-            setInputBottomLineColor((TextView) findViewById(R.id.nInput), R.styleable.CustomInput_ci_botton_line_default, R.color.CustomInput_line_default);
-            setUnFocusState(view);
+           setUnFocusState(nView);
         }
     }
 
@@ -114,7 +106,9 @@ public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocus
         mInput.setText(nText);
     }
 
-    public void setHint(String nText) { mInput.setHint(nText);}
+    public void setHint(String nText) {
+        mInput.setHint(nText);
+    }
 
     /**
      * Set a new error message and shows-up
@@ -155,84 +149,75 @@ public class CustomInput extends BaseCustom_LinearLayout implements View.OnFocus
         return mInputKeyboard;
     }
 
-    public void setEditTextParamiters(TextView textView){
+    public void setEditTextParameters(TextView nTextView) {
         mInputKeyboard = mTypedarray.getString(R.styleable.CustomInput_ci_keyboard);
-        mScrollbar = mTypedarray.getString(R.styleable.CustomInput_ci_scrollbars);
-        textView.setVisibility(View.VISIBLE);
-        setPadding(textView,R.styleable.CustomInput_ci_padding,getResources().getInteger(R.integer.ci_padping));
-        setStyle(textView, R.styleable.CustomInput_ci_style, R.style.ci_default_style);
-        setFontFamily(textView, mTypedarray.getString(R.styleable.CustomInput_ci_font), false);
-        setInputHint(textView, R.styleable.CustomInput_ci_hint);
-        textView.setOnFocusChangeListener(this);
-        if(textView==mInput){
-            setInputBottomLineColor(textView, R.styleable.CustomInput_ci_botton_line_default, R.color.CustomInput_line_default);
 
-        }
+        setPadding(nTextView, R.styleable.CustomInput_ci_padding, getResources().getInteger(R.integer.ci_padping));
+        setStyle(nTextView, R.styleable.CustomInput_ci_style, R.style.ci_default_style);
+        setFontFamily(nTextView, mTypedarray.getString(R.styleable.CustomInput_ci_font), false);
+        setInputHint(nTextView, R.styleable.CustomInput_ci_hint);
+        setDefaultState(nTextView);
+        nTextView.setOnFocusChangeListener(this);
 
-
-        if (mInputKeyboard!=null) {
+       if (mInputKeyboard != null) {
             switch (mInputKeyboard) {
                 case "1":
-                    textView.setInputType(InputType.TYPE_CLASS_TEXT);
+                    nTextView.setInputType(InputType.TYPE_CLASS_TEXT);
                     break;
                 case "12":
-                    textView.setInputType(InputType.TYPE_CLASS_TEXT);
+                    nTextView.setInputType(InputType.TYPE_CLASS_TEXT);
                     break;
                 case "2":
-                    textView.setInputType(InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS);
+                    nTextView.setInputType(InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS);
                     break;
                 case "3":
-                    textView.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    nTextView.setInputType(InputType.TYPE_CLASS_NUMBER);
                     break;
                 case "4":
-                    textView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    nTextView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                     break;
                 case "5":
-                    textView.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    nTextView.setInputType(InputType.TYPE_CLASS_NUMBER);
                     break;
             }
         }
 
-        if(mTypedarray.getString(R.styleable.CustomInput_ci_imei_option)!=null) {
+        if (mTypedarray.getString(R.styleable.CustomInput_ci_imei_option) != null) {
             switch (mTypedarray.getString(R.styleable.CustomInput_ci_imei_option)) {
                 case "1":
-                    textView.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+                    nTextView.setImeOptions(EditorInfo.IME_ACTION_NEXT);
                     break;
                 case "0":
-                    textView.setImeOptions(EditorInfo.IME_ACTION_DONE);
-                    break;
-                case "2":
-                    mInputMultiLine.setSingleLine(false);
-                    mInputMultiLine.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+                    nTextView.setImeOptions(EditorInfo.IME_ACTION_DONE);
                     break;
             }
         }
     }
 
-    public void setFocusState(View editText){
-        Log.d("adaw","setFocusState");
+    public void setFocusState(View editText) {
+        Log.d("adaw", "setFocusState");
         setBorderColorAndRadius(editText,
-                R.styleable.CustomInput_ci_backgroundColor,mDefaultBackgroundColor,
-                R.styleable.CustomInput_ci_border_radius,mDefaultBorderRadius,
-                R.styleable.CustomInput_ci_border_stroke,mDefaultBorderStroke,
-                R.styleable.CustomInput_ci_border_pressed,mDefaultBorderColorFocus);
+                R.styleable.CustomInput_ci_backgroundColor, mDefaultBackgroundColor,
+                R.styleable.CustomInput_ci_border_radius, mDefaultBorderRadius,
+                R.styleable.CustomInput_ci_border_stroke, mDefaultBorderStroke,
+                R.styleable.CustomInput_ci_border_pressed, mDefaultBorderColorFocus);
     }
 
-    public void setUnFocusState(View editText){
-        Log.d("adaw","setUnFocusState");
+    public void setUnFocusState(View editText) {
+        Log.d("adaw", "setUnFocusState");
         setBorderColorAndRadius(editText,
-                R.styleable.CustomInput_ci_backgroundColor,mDefaultBackgroundColor,
-                R.styleable.CustomInput_ci_border_radius,mDefaultBorderRadius,
-                R.styleable.CustomInput_ci_border_stroke,mDefaultBorderStroke,
-                R.styleable.CustomInput_ci_border_unpressed,mDefaultBorderColorUnFocus);
+                R.styleable.CustomInput_ci_backgroundColor, mDefaultBackgroundColor,
+                R.styleable.CustomInput_ci_border_radius, mDefaultBorderRadius,
+                R.styleable.CustomInput_ci_border_stroke, mDefaultBorderStroke,
+                R.styleable.CustomInput_ci_border_unpressed, mDefaultBorderColorUnFocus);
     }
 
     public void setDefaultState(View editText) {
-        Log.d("adaw","setDefaultState");
+        Log.d("adaw", "setDefaultState");
         setBorderColorAndRadius(editText,
-                R.styleable.CustomInput_ci_backgroundColor,mDefaultBackgroundColor,
-                R.styleable.CustomInput_ci_border_radius,mDefaultBorderRadius,
-                R.styleable.CustomInput_ci_border_stroke,mDefaultBorderStroke,
-                R.styleable.CustomInput_ci_border_unpressed,mDefaultBorderColorUnFocus);
+                R.styleable.CustomInput_ci_backgroundColor, mDefaultBackgroundColor,
+                R.styleable.CustomInput_ci_border_radius, mDefaultBorderRadius,
+                R.styleable.CustomInput_ci_border_stroke, mDefaultBorderStroke,
+                R.styleable.CustomInput_ci_border_unpressed, mDefaultBorderColorUnFocus);
     }
 }
