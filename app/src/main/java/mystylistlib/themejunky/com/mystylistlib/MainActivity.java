483c2b189bf67246383f8a71e4bd7c64000b291c
Bejan Corneliu <bejancorneliu@gmail.com>
2018-04-04 15:57:26 +0300

    - styles review

--- app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
+++ app/src/main/java/mystylistlib/themejunky/com/mystylistlib/MainActivity.java
@@ -6,22 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
 
-import com.themejunky.personalstylerlib.InitCaligraphy;
-import com.themejunky.personalstylerlib.customviews.CustomButton;
 import com.themejunky.personalstylerlib.customviews.CustomButtonBorder;
-import com.themejunky.personalstylerlib.customviews.CustomButtonBorderImage;
 import com.themejunky.personalstylerlib.customviews.CustomInput;
 import com.themejunky.personalstylerlib.customviews.CustomTextSwitch;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity  implements CustomButtonBorder.CustomButtonBorderInterface {
 
@@ -35,7 +24,6 @@ public class MainActivity extends AppCompatActivity  implements CustomButtonBord
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("Catamaran-Regular.ttf").setFontAttrId(R.attr.fontPath).build());
        /* mSpinner=  findViewById(R.id.nSpinnerTest);
         List<String> list = new ArrayList<String>();
         list.add("list 1");
@@ -55,11 +43,6 @@ public class MainActivity extends AppCompatActivity  implements CustomButtonBord
 
 
 
-    }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
     @Override
