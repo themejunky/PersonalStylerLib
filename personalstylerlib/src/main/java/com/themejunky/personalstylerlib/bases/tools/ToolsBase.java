9f72424ab397a2959052e90e9de25faf84ae6d2c
Bejan Corneliu <bejancorneliu@gmail.com>
2018-04-05 11:28:41 +0300

    - Text CustomBaseActivity(13)

--- personalstylerlib/src/main/java/com/themejunky/personalstylerlib/bases/tools/ToolsBase.java
+++ personalstylerlib/src/main/java/com/themejunky/personalstylerlib/bases/tools/ToolsBase.java
@@ -1,6 +1,7 @@
 package com.themejunky.personalstylerlib.bases.tools;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,17 +14,25 @@ import com.themejunky.personalstylerlib.R;
 import com.themejunky.personalstylerlib.bases.model.Item;
 import com.themejunky.personalstylerlib.customviews.CustomInput;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ToolsBase {
 
     static Context mContext;
     static Tools mInstance = null;
 
-    int mRowHeight;
+    public Activity mActivity;
+    public View.OnClickListener mViewOnClickListener;
+
+    public int mRowHeight;
+
 
 
     public Locale getLocale() {
@@ -36,6 +45,54 @@ public class ToolsBase {
         return mRowHeight;
     }
 
+    public void setActivity(Activity nActivity) {
+        this.mActivity = nActivity;
+    }
+
+    public void setViewListener(View.OnClickListener nViewOnClickListener) {
+        this.mViewOnClickListener = nViewOnClickListener;
+    }
+    protected String getString(int nStringResources) {
+        return mContext.getResources().getString(nStringResources);
+    }
+
+    protected int getColor(int nColorResources) {
+        return mContext.getResources().getColor(nColorResources);
+    }
+
+    public boolean isEmailValid(String nEmail) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(nEmail);
+        return matcher.matches();
+    }
+
+    public boolean isPhoneValid(String nPhone) {
+        return android.util.Patterns.PHONE.matcher(nPhone).matches();
+    }
+
+    public String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2) h = "0".concat(h);
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
     /**
      * Find and select the desired value
