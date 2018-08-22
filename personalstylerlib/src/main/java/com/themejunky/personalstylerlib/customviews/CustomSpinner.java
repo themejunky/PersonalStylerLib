d3642b08be9d017de0d41b161090c170f38477bb
Bejan Corneliu <bejancorneliu@gmail.com>
2018-08-22 15:23:44 +0300

    -bug4

--- personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomSpinner.java
+++ personalstylerlib/src/main/java/com/themejunky/personalstylerlib/customviews/CustomSpinner.java
@@ -2,6 +2,7 @@ package com.themejunky.personalstylerlib.customviews;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.themejunky.personalstylerlib.R;
 
@@ -13,10 +14,14 @@ public class CustomSpinner extends BaseCustom_LinearLayout {
     public CustomSpinner(Context nContext, AttributeSet nAttrs) {
         super(nContext, nAttrs);
 
-        mContext = nContext;
+        try {
+            mContext = nContext;
 
-        mTypedarray = nContext.obtainStyledAttributes(nAttrs, R.styleable.CustomInput);
-        inflate(nContext, R.layout.custom_spinner, this);
+            mTypedarray = nContext.obtainStyledAttributes(nAttrs, R.styleable.CustomInput);
+            inflate(nContext, R.layout.custom_spinner, this);
+        } catch (Exception e) {
+            Log.d("eroare_naspa","2 : "+e.getMessage());
+        }
 
     }
 }
